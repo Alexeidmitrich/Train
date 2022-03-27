a5d9fe03dfbc75cc05593686200d1c08eeaccdab
Aleksei Gorshkov <alexeidmitrich@gmail.com>
2022-03-27 09:16:57 +0300

    Attempt to remake the project on your comments

--- src/train/Train.java
+++ src/train/Train.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Train{
+public class Train {
+
     List<Schedule> scheduleList = new ArrayList<>();
 
-    public void addSchedule(int number, String pointOfDeparture, String destination, String date, String time){
+    public void addSchedule(int number, String pointOfDeparture, String destination, String date, String time) {
         Schedule schedule = new Schedule(number, pointOfDeparture, destination, date, time);
         scheduleList.add(schedule);
     }
@@ -19,24 +20,73 @@ public class Train{
         }
     }
 
+    public void enterUserNumber(int number) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите параметры поезда");
+        int b = sc.nextInt();
+        for (int i = 0; i < scheduleList.size(); i++) {
+            Schedule train = scheduleList.get(i);
+            if (b == number) {
+                train.printInformation();
+                break;
+            }
+        }
+    }
+    public void enterUserPointOfDeparture(String pointOfDeparture) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите параметры поезда");
+        String b = sc.nextLine();
+        for (int i = 0; i < scheduleList.size(); i++) {
+            Schedule train = scheduleList.get(i);
+            if (b == pointOfDeparture) {
+                train.printInformation();
+                break;
+            }
+        }
+    }
+    public void enterUserDestination(String destination) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите параметры поезда");
+        String b = sc.nextLine();
+        for (int i = 0; i < scheduleList.size(); i++) {
+            Schedule train = scheduleList.get(i);
+            if (b == destination) {
+                train.printInformation();
+                break;
+            }
+        }
+    }
+    public void enterUserDate(String date) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите параметры поезда");
+        String b = sc.nextLine();
+        for (int i = 0; i < scheduleList.size(); i++) {
+            Schedule train = scheduleList.get(i);
+            if (b == date) {
+                train.printInformation();
+                break;
+            }
+        }
+    }
+    public void enterUserTime(int number, String pointOfDeparture, String destination, String date, String time) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите параметры поезда");
+        String b = sc.nextLine();
+        for (int i = 0; i < scheduleList.size(); i++) {
+            Schedule train = scheduleList.get(i);
+            if (b == time) {
+                train.printInformation();
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Train a = new Train();
-        a.addSchedule(10,"Sankt-Petersburg", "Tula", "28.03.2022","11*00");
-        a.addSchedule(108,"Sankt-Petersburg", "Dno", "29.03.2022","01*15");
-        a.addSchedule(44,"Sankt-Petersburg", "Pskov", "27.03.2022","19*45");
-        a.printAllSchedule();
-    }
-    public Object Departure() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите номер поезда");
-        int n = in.nextInt();
-        if (n == 10) {
-            return scheduleList.get(0);
-        } else if (n == 108) {
-            return scheduleList.get(1);
-        } else if (n == 44) {
-            return scheduleList.get(2);
-        }
-        return null;
+        a.addSchedule(10, "Sankt-Petersburg", "Tula", "28.03.2022", "11*00");
+        a.addSchedule(108, "Sankt-Petersburg", "Dno", "29.03.2022", "01*15");
+        a.addSchedule(44, "Sankt-Petersburg", "Pskov", "27.03.2022", "19*45");
+
+
     }
 }
